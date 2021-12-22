54603bc057ac3201abbc870fa7ac79d87a660af0
carlo <ecaliwanag@gmail.com>
2021-12-23 02:04:25 +0800

    update webpack; use web3 cdn

---
+++ truffle-config.js
@@ -0,0 +1,20 @@
+var HDWalletProvider = require('truffle-hdwallet-provider');
+var mnemonic =
+  'render bachelor above exact flash cloth license wine guard edit sugar enhance';
+
+module.exports = {
+  networks: {
+    development: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, 'http://127.0.0.1:9545/', 0, 50);
+      },
+      network_id: '*',
+      gas: 9999999,
+    },
+  },
+  compilers: {
+    solc: {
+      version: '^0.4.24',
+    },
+  },
+};
