d5d6e340aea08d7434f3216252aa6ee6a59cece6
carlo <ecaliwanag@gmail.com>
2022-01-02 06:42:28 +0800

    lowered gas and gas price for deployment

--- truffle-config.js
+++ truffle-config.js
@@ -6,10 +6,11 @@ module.exports = {
   networks: {
     development: {
       provider: function () {
-        return new HDWalletProvider(mnemonic, 'http://127.0.0.1:9545/', 0, 50);
+        return new HDWalletProvider(mnemonic, 'http://127.0.0.1:8545/', 0, 50);
       },
       network_id: '*',
-      gas: 9999999,
+      gas: 6000000,
+      gasPrice: 0,
     },
   },
   compilers: {
